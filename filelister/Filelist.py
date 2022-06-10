f4df5816783cf42e77a4a3a9d72d87f96dfa2e40
simon-burke <simonburke1@mac.com>
2022-06-09 19:55:24 -0700

    updatated Filelist and read_filelist

--- filelister/Filelist.py
+++ filelister/Filelist.py
@@ -1,3 +1,26 @@
+import os
+
+
 class Filelist:
-    def __init__(self):
-        pass
+    def __init__(self, data=None):
+        self.accept_input(data)
+
+    def accept_input(self, data):
+        if type(data) == list:
+            return create_from_list(data)
+        elif type(data) == dict:
+            create_from_dict(data)
+
+    def create_from_list(self, data):
+        file_data = []
+        for file in data:
+            if file[0] == '/':
+                file_data.append(file)
+            else:
+                file_data.append(self.relative_to_abs(file_abspath))
+
+    def create_from_dict(data):
+        pass #Why the fuck would you do this and how would you implement it?
+
+    def relative_to_abs(path):
+        return os.path.abspath(os.path.join(os.getcwd(), path))
--- filelister/read_filelist.py
+++ filelister/read_filelist.py
@@ -1,2 +1,32 @@
+import os
+import Filelist as fs
+
 def read_filelist(infile):
-    pass
+    with open(infile) as f:
+        first_byte = f.read(1)
+        if first_byte == '/':
+            with open(infile) as cocks:
+                return [line.rstrip() for line in cocks]
+            # fpaths = [line.rstrip() for line in open(infile)]
+        else:
+            with open(infile) as cocks:
+                # working directory of the infile
+                return [os.path.abspath(os.path.join(infile, line.rstrip())) for line in cocks]
+                # return [line.rstrip() for line in cocks]
+            # fpaths = [os.path.abspath(os.path.join(infile, line.rstrip())) for line in open(infile)]
+        raise
+        # return fpaths
+        # ../../../data.txt
+        # /Users/simon/files/data.txt
+        # flist.save()
+
+
+def check_duplicate_path(fpaths):
+    fpath_set = set(fpaths)
+    if len(fpath_set) != len(fpaths):
+        raise Error
+
+
+if __name__ == "__main__":
+    flist = read_filelist('../tests/filelists/rel_filelist.txt')
+    print(flist)
