e8d3c0e8299332753fe53767ce5e4fc745c6b49f
burkecp <burkechristianp@gmail.com>
2022-06-09 20:33:28 -0700

    debugged Filelist

--- filelister/Filelist.py
+++ filelister/Filelist.py
@@ -3,24 +3,38 @@ import os
 
 class Filelist:
     def __init__(self, data=None):
-        self.accept_input(data)
+        if data:
+            self.data = self.accept_input(data)
+        elif type(data) not in [list, set, None]:
+            raise TypeError(f'{type(data)} is an invalid input type.')
+        else:
+            self.data = None
+
+    def __str__(self):
+        if self.data:
+            str_out = ''
+            for fname in self.data:
+                str_out += fname + '\n'
+            return str_out
+        else:
+            return 'Filelist is empty'
 
     def accept_input(self, data):
         if type(data) == list:
-            return create_from_list(data)
-        elif type(data) == dict:
-            create_from_dict(data)
+            return self.create_from_list(data)
+        elif type(data) == set:
+            self.create_from_dict(data)
+        elif type(data) == None:
+            return None
 
     def create_from_list(self, data):
-        file_data = []
-        for file in data:
-            if file[0] == '/':
-                file_data.append(file)
-            else:
-                file_data.append(self.relative_to_abs(file_abspath))
+        if data[0][0] == '/':
+            return data
+        else:
+            return [self.relative_to_abs(fname) for fname in data]
 
-    def create_from_dict(data):
-        pass #Why the fuck would you do this and how would you implement it?
+    def create_from_set(self, data):
+        pass
 
-    def relative_to_abs(path):
+    def relative_to_abs(self, path):
         return os.path.abspath(os.path.join(os.getcwd(), path))
